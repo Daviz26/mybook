7ad1bef9d8ea355d664fc1aa7f056fc11adf33b9
Daviz Charles <pete_iz@hotmail.com>
2016-06-20 13:53:13 +0000

    My initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable
+         
+  def full_name
+    return "#{first_name} #{last_name}".strip if (first_name || last_name)
+    # if the user don't have a first or last name show as anonymous
+    "Anonymous"
+  end
+
+end
